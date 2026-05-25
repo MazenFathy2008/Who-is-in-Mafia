3e946a1ab37552aadf0215cad834869c617ee4a6
Mazen Fathy <mazenfathy841@gmail.com>
2026-05-25 14:09:05 +0300

    preparing color pallet and fonts.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+  return (
+    <>
+      <div
+        className="
+      w-full 
+      h-screen
+      bg-background
+      border-Im2
+      border-10
+      rounded-4xl
+      text-font
+      font-Jungle
+      flex
+      justify-center
+      items-center
+      "
+      ></div>
+    </>
+  );
+}
