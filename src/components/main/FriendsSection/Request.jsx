1b4f99197d0a6eaf4a631fd7cf5df8131d8d9336
mazenDev <mazenfathy841@gmail.com>
2026-07-12 10:49:27 +0300

    making reqeusts section functionable and adding masseage when there isn't any request

--- src/components/main/FriendsSection/Request.jsx
+++ src/components/main/FriendsSection/Request.jsx
@@ -1,15 +1,23 @@
-import { ref, onChildAdded, onChildRemoved } from "firebase/database";
+import { ref, onChildAdded, onChildRemoved, remove } from "firebase/database";
 import { db } from "../../../config/firebase";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import acceptRequest from "./utils/acceptReqeust";
 import { buttonStyles } from "../styles";
 import { AnimatePresence, motion } from "motion/react";
+import {useOutletContext} from "react-router-dom"
 export default function Requests() {
   const [requests, setRequests] = useState(null);
   const uid = useParams().userId;
-  const handleClick = (friendId) => {
+  const { back } = useOutletContext();
+  const handleClickAccept = (friendId) => {
     acceptRequest(uid, friendId);
+  };
+  const handleClickReject = (friendId) => {
+    const myRef = ref(db, `users/${uid}/requests/${friendId}`);
+    const friendRef = ref(db, `users/${friendId}/sentRequests/${uid}`);
+    remove(myRef);
+    remove(friendRef);
   };
   const requestRef = ref(db, `users/${uid}/requests`);
   useEffect(() => {
@@ -47,9 +55,19 @@ export default function Requests() {
         >
           {requests[requestId].username}
           <button
-            className={buttonStyles + "w-25/100"}
+            className={buttonStyles.replace("bg-font", "bg-Im1") + "w-25/100"}
             onClick={() => {
-              handleClick(requestId);
+              handleClickReject(requestId);
+            }}
+          >
+            Reject
+          </button>
+          <button
+            className={
+              buttonStyles.replace("bg-font", "bg-green-500") + "w-25/100"
+            }
+            onClick={() => {
+              handleClickAccept(requestId);
             }}
           >
             Accept
@@ -61,11 +79,44 @@ export default function Requests() {
     <div
       className="
   w-full h-full
-  flex items-center justify-center
   "
     >
-      <ul className="w-full h-full overflow-y-auto overflow-x-hidden p-5 flex flex-col gap-[10%]">
-        <AnimatePresence>{requestsList}</AnimatePresence>
+      <button
+        className="
+        underline
+        text transition
+        duration-200
+        hover:scale-110
+        active:scale-95
+        text-2xl
+        hover:[text-shadow:1px_1px_5px_white]
+      "
+        onClick={() => {
+          back();
+        }}
+      >
+        back
+      </button>
+      <ul
+        className={
+          "w-full h-full overflow-y-auto overflow-x-hidden p-5 flex flex-col gap-[10%]"
+        }
+      >
+        <AnimatePresence>
+          {requests ? (
+            requestsList
+          ) : (
+            <motion.p
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 100 }}
+              transition={{ duration: 0.3 }}
+              className="w-full h-full flex items-center justify-center text-2xl"
+            >
+              You don't have any Requests Right now 
+            </motion.p>
+          )}
+        </AnimatePresence>
       </ul>
     </div>
   );
