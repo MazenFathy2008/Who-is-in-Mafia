afe0875694749180ade5707942dfad0a2359e60f
mazenDev <mazenfathy841@gmail.com>
2026-07-13 18:35:31 +0300

    styling join face

--- src/components/main/Game/JoinRoom.jsx
+++ src/components/main/Game/JoinRoom.jsx
@@ -1,12 +1,12 @@
 import { motion } from "motion/react";
 import { useState } from "react";
-export default function CreatRoom({ flipped, setFlipped }) {
-
+import { buttonStyles } from "../styles";
+export default function JoinRoom({ flipped, setFlipped }) {
   return (
     <motion.div
       initial={{
         rotateY: 180,
-        opacity:0,
+        opacity: 0,
       }}
       animate={{
         opacity: flipped ? 1 : 0,
@@ -29,9 +29,73 @@ export default function CreatRoom({ flipped, setFlipped }) {
     justify-between
     "
     >
-      back
-      <button onClick={()=>{setFlipped(false)}} type="button">
-        CreateRoom
+      <h1>Join room</h1>
+      <div className="relative flex w-full items-center justify-center h-fit">
+        <input
+          type="text"
+          id="roomId"
+          placeholder=""
+          className="border-3
+        border-Im1
+        focus:border-Im2
+        transition
+        duration-200
+        w-full
+        outline-0
+        p-2
+        box-border
+        rounded-2xl
+        peer
+        "
+        />
+        <label
+          htmlFor="roomId"
+          className="       
+          absolute
+          z-100
+          select-none
+          top-1/2
+          -translate-y-1/2
+          left-4
+          md:left-1/2
+          md:-translate-x-1/2
+          text-font
+          opacity-50
+          transition-all
+          duration-200
+          peer-focus:opacity-100
+          peer-focus:-top-5
+          peer-focus:left-5
+          md:peer-focus:left-1/2
+          peer-focus:sm:left-10 
+          peer-focus:scale-125
+          peer-focus:sm:scale-150
+          peer-not-placeholder-shown:opacity-100
+          peer-not-placeholder-shown:-top-5
+          peer-not-placeholder-shown:left-5
+          md:peer-not-placeholder-shown:left-1/2
+          peer-not-placeholder-shown:sm:left-10 
+          peer-not-placeholder-shown:scale-125 
+          peer-not-placeholder-shown:sm:scale-150"
+        >
+          Enter room Id
+        </label>
+      </div>
+      <button className={buttonStyles+"w-full"} type="button">Join Now</button>
+      <button
+        onClick={() => {
+          setFlipped(false);
+        }}
+        type="button"
+        className={`z-10 cursor-pointer
+  hover:scale-125
+  transition
+  duration-200
+  border-b-xl
+  border-b-font
+  underline`}
+      >
+        Do you want to Create Room
       </button>
     </motion.div>
   );
