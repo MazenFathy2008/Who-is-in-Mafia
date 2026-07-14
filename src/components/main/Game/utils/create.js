13a37325ef1ea31bd92cf73b8db1bf4dc1b755d8
mazenDev <mazenfathy841@gmail.com>
2026-07-14 16:27:55 +0300

    sending invite for each friend

--- src/components/main/Game/utils/create.js
+++ src/components/main/Game/utils/create.js
@@ -5,15 +5,25 @@ const createNewRoom = async (hostId, invitedFriends = {}) => {
   const roomRefrence = ref(db, `rooms/${roomId}/`);
   const hostDataRefrence = ref(db, `users/${hostId}/Profile`);
   const userData = await get(hostDataRefrence);
-  const invetationsRef = ref(db,`rooms/${roomId}/invetations`)
+  const invetationsRef = ref(db, `rooms/${roomId}/invetations`);
   set(roomRefrence, {
     host: { id: hostId, ...userData.val() },
     players: {
-      [hostId]: { id: hostId,isHost:true, ...userData.val(), },
+      [hostId]: { id: hostId, isHost: true, ...userData.val() },
     },
   });
-  set(invetationsRef,{
-    ...invitedFriends
-  })
+  set(invetationsRef, {
+    ...invitedFriends,
+  });
+  Object.keys(invitedFriends).forEach(async (friend) => {
+    const friendRef = ref(db, `users/${invitedFriends[friend].id}/Play/invetations/${roomId}/`);
+    set(friendRef, {
+      roomId: roomId,
+      senderData: {
+        id: hostId,
+        ...userData.val(),
+      },
+    });
+  });
 };
 export default createNewRoom;
