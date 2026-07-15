d2443cc17b84cd4c9168c4d63fe8dca1f8d17d6a
mazenDev <mazenfathy841@gmail.com>
2026-07-16 01:39:53 +0300

    Making game Friends list to be in its own component to be used later

---
+++ src/components/global/GameFriendsList.jsx
@@ -0,0 +1,96 @@
+import { AnimatePresence, motion } from "motion/react";
+import { onValue, ref } from "firebase/database";
+import { db } from "../../config/firebase";
+import useStopLoader from "../../hooks/useStopLoader";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+export default function GameFriendsLis({
+  setInvitedFriends,
+  disabledButtonstyles,
+  buttonStyles,
+}) {
+  const [friends, setFriends] = useState(null);
+  const stopLoader = useStopLoader();
+  const { userId: uid } = useParams();
+  const handleInvite = (data) => {
+    setInvitedFriends((prev) => {
+      return { ...(prev || {}), [data.id]: { ...data, disabled: null } };
+    });
+    setFriends((prev) => {
+      return prev.map((friend) => {
+        if (friend.id == data.id) {
+          return {
+            ...friend,
+            disabled: true,
+          };
+        }
+        return friend;
+      });
+    });
+  };
+  useEffect(() => {
+    const refrence = ref(db, `users/${uid}/Friends`);
+    const unsub = onValue(refrence, (snapshot) => {
+      const data = snapshot.val();
+      setFriends((prev) => {
+        if (!data) return null;
+        return [
+          ...(prev || []),
+          ...Object.keys(data).map((key) => {
+            return {
+              disabled: false,
+              id: key,
+              ...data[key],
+            };
+          }),
+        ];
+      });
+      stopLoader();
+    });
+    return unsub;
+  }, []);
+  const friendsList = friends ? (
+    friends.map((friend) => {
+      return (
+        <li
+          key={friend.id}
+          className="
+        flex w-full justify-between
+        "
+        >
+          {friend.username}
+          <div
+            onClick={() => {
+              friend.disabled ? null : handleInvite(friend);
+            }}
+            className={
+              friend.disabled
+                ? disabledButtonstyles + "w-25/100 cursor-not-allowed"
+                : buttonStyles.replace(
+                    "bg-font",
+                    "bg-green-500 cursor-pointer",
+                  ) + "w-25/100"
+            }
+          >
+            Invite
+          </div>
+        </li>
+      );
+    })
+  ) : (
+    <motion.p
+      initial={{ opacity: 0, x: 50 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: 100 }}
+      transition={{ duration: 0.3 }}
+      className="w-full h-full flex items-center justify-center text-lg"
+    >
+      You don't have any Friends till now ....
+    </motion.p>
+  );
+  return (
+    <ul className="w-full h-1/2 border-4 rounded-2xl p-4 gap-3 flex flex-col">
+      <AnimatePresence>{friendsList}</AnimatePresence>
+    </ul>
+  );
+}
--- src/components/main/Game/CreateRoom.jsx
+++ src/components/main/Game/CreateRoom.jsx
@@ -1,95 +1,15 @@
 import { motion } from "motion/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { buttonStyles, disabledButtonstyles } from "../styles";
-import { onValue, ref } from "firebase/database";
 import { useParams } from "react-router-dom";
-import { db } from "../../../config/firebase";
 import createNewRoom from "./utils/create";
-import useStopLoader from "../../../hooks/useStopLoader";
+import GameFriendsLis from "../../global/GameFriendsList";
 export default function CreatRoom({ flipped, setFlipped }) {
-  const [friends, setFriends] = useState(null);
   const [invitedFriends, setInvitedFriends] = useState();
   const uid = useParams().userId;
-  const stopLoader = useStopLoader();
-  useEffect(() => {
-    const refrence = ref(db, `users/${uid}/Friends`);
-    const unsub = onValue(refrence, (snapshot) => {
-      const data = snapshot.val();
-      setFriends((prev) => {
-        if (!data) return null;
-        return [
-          ...(prev || []),
-          ...Object.keys(data).map((key) => {
-            return {
-              disabled: false,
-              id: key,
-              ...data[key],
-            };
-          }),
-        ];
-      });
-      stopLoader();
-    });
-    return unsub;
-  }, []);
   const handleClick = () => {
     createNewRoom(uid, invitedFriends);
   };
-  const handleInvite = (data) => {
-    setInvitedFriends((prev) => {
-      return { ...(prev || {}), [data.id]: { ...data, disabled: null } };
-    });
-    setFriends((prev) => {
-      return prev.map((friend) => {
-        if (friend.id == data.id) {
-          return {
-            ...friend,
-            disabled: true,
-          };
-        }
-        return friend;
-      });
-    });
-  };
-  const friendsList = friends ? (
-    friends.map((friend) => {
-      return (
-        <li
-          key={friend.id}
-          className="
-        flex w-full justify-between
-        "
-        >
-          {friend.username}
-          <div
-            onClick={() => {
-              friend.disabled ? null : handleInvite(friend);
-            }}
-            className={
-              friend.disabled
-                ? disabledButtonstyles + "w-25/100 cursor-not-allowed"
-                : buttonStyles.replace(
-                    "bg-font",
-                    "bg-green-500 cursor-pointer",
-                  ) + "w-25/100"
-            }
-          >
-            Invite
-          </div>
-        </li>
-      );
-    })
-  ) : (
-    <motion.p
-      initial={{ opacity: 0, x: 50 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 100 }}
-      transition={{ duration: 0.3 }}
-      className="w-full h-full flex items-center justify-center text-lg"
-    >
-      You don't have any Friends till now ....
-    </motion.p>
-  );
   return (
     <motion.div
       initial={{
@@ -117,9 +37,11 @@ export default function CreatRoom({ flipped, setFlipped }) {
     "
     >
       <h1>Create Room</h1>
-      <ul className="w-full h-1/2 border-4 rounded-2xl p-4 gap-3 flex flex-col">
-        {friendsList}
-      </ul>
+      <GameFriendsLis
+        setInvitedFriends={setInvitedFriends}
+        buttonStyles={buttonStyles}
+        disabledButtonstyles={disabledButtonstyles}
+      />
       <button
         type="button"
         className={buttonStyles + "w-full"}
