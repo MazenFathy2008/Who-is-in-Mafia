0b75d93243602cfc5fcbd7cd8530cc1c8302e4c4
mazenDev <mazenfathy841@gmail.com>
2026-07-16 17:51:38 +0300

    trying to fix Friends invite list

--- src/components/global/GameFriendsList.jsx
+++ src/components/global/GameFriendsList.jsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from "motion/react";
-import { onValue, ref } from "firebase/database";
+import { onValue, ref, get } from "firebase/database";
 import { db } from "../../config/firebase";
 import useStopLoader from "../../hooks/useStopLoader";
 import { useEffect, useState } from "react";
@@ -11,6 +11,7 @@ export default function GameFriendsLis({
   isInlobby = false,
 }) {
   const [friends, setFriends] = useState(null);
+  const [players, setPlayers] = useState(null);
   const stopLoader = useStopLoader();
   const { userId: uid, roomId } = useParams();
   const handleInvite = (data) => {
@@ -30,82 +31,83 @@ export default function GameFriendsLis({
     });
   };
   useEffect(() => {
-    const refrence = ref(db, `users/${uid}/Friends`);
-    const unsub = onValue(refrence, (snapshot) => {
-      const data = snapshot.val();
-      setFriends((prev) => {
-        if (!data) return null;
-        return [
-          ...Object.keys(data).map((key) => {
-            return {
-              disabled: false,
-              id: key,
-              ...data[key],
-            };
-          }),
-        ];
-      });
-      stopLoader();
+  if (!isInlobby) return;
+
+  const roomRef = ref(db, `rooms/${roomId}/players`);
+
+  const unsub = onValue(roomRef, (snapshot) => {
+    setPlayers(snapshot.val() || {});
+  });
+
+  return unsub;
+}, [isInlobby, roomId]);
+  useEffect(() => {
+    const roomRef = ref(db, `rooms/${roomId}/players`);
+    const unsub = onValue(roomRef, (snapshot) => {
+      console.log(friends);
+      if (friends) {
+        const refrence = ref(db, `users/${uid}/Friends`);
+        const newfriends = [];
+        get(refrence).then((friends) => {
+          Object.keys(friends.val()).forEach((id) => {
+            if (!snapshot.val()[id]) {
+              newfriends.push({
+                disabled: false,
+                id: key,
+                ...friends[id],
+              });
+            }
+          });
+        });
+        setFriends(newfriends);
+      }
     });
     return unsub;
   }, []);
-
-  useEffect(() => {
-    if (!isInlobby) return;
-
-    const roomRef = ref(db, `rooms/${roomId}/players`);
-
-    const unsub = onValue(roomRef, (snapshot) => {
-      const players = snapshot.val() || {};
-
-      setFriends((prev) => {
-        if (!prev) return prev;
-        return prev.filter((friend) => !players[friend.id]);
-      });
-    });
-
-    return unsub;
-  }, [isInlobby, roomId]);
-
-  const friendsList = friends && friends?.length>0 ? (
-    friends.map((friend) => {
-      return (
-        <li
-          key={friend.id}
-          className="
-        flex w-full justify-between
-        "
-        >
-          {friend.username}
-          <div
-            onClick={() => {
-              friend.disabled ? null : handleInvite(friend);
-            }}
-            className={
-              friend.disabled
-                ? disabledButtonstyles + "w-25/100 cursor-not-allowed"
-                : buttonStyles.replace(
-                    "bg-font",
-                    "bg-green-500 cursor-pointer",
-                  ) + "w-25/100"
-            }
+  const friendsList =
+    friends && friends?.length > 0 ? (
+      friends.map((friend) => {
+        return (
+          <li
+            key={friend.id}
+            className="
+          flex w-full justify-between
+          "
           >
-            Invite
-          </div>
-        </li>
-      );
-    })
-  ) : (
-    <motion.p
-      initial={{ opacity: 0, x: 50 }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: 100 }}
-      transition={{ duration: 0.3 }}
-      className="w-full h-full flex items-center justify-center text-lg"
-    >
-      You don't have any Friends till now ....
-    </motion.p>
-  );
+            {friend.username}
+            <div
+              onClick={() => {
+                isInlobby
+                  ? null
+                  : friend.disabled
+                    ? null
+                    : handleInvite(friend);
+              }}
+              className={
+                friend.disabled
+                  ? disabledButtonstyles + "w-25/100 cursor-not-allowed"
+                  : buttonStyles.replace(
+                      "bg-font",
+                      "bg-green-500 cursor-pointer",
+                    ) + "w-25/100"
+              }
+            >
+              Invite
+            </div>
+          </li>
+        );
+      })
+    ) : (
+      <motion.p
+        initial={{ opacity: 0, x: 50 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 100 }}
+        transition={{ duration: 0.3 }}
+        className="w-full h-full flex items-center justify-center text-lg"
+      >
+        You don't have any Friends till now ....
+      </motion.p>
+    );
   return (
     <ul
       className={`w-full ${isInlobby ? "h-full" : "h-1/2"} border-4 rounded-2xl p-4 gap-3 flex flex-col`}
