244059224ba49bf2a9ea013b9be1ad1bf2d278af
mazenDev <mazenfathy841@gmail.com>
2026-07-19 03:11:30 +0300

    makin smoth transtion between pages and showing the  role of the player

--- src/components/Game/lobby/user.jsx
+++ src/components/Game/lobby/user.jsx
@@ -1,18 +1,20 @@
 import { onValue, ref, remove, set, get } from "firebase/database";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { createPortal } from "react-dom";
+import { motion } from "motion/react";
 import Invitations from "./sections/invetation";
 import { db } from "../../../config/firebase";
 import Players from "./sections/Players";
 export default function UserLobby() {
   const { roomId, userId } = useParams();
   const navigate = useNavigate();
+  const [isStarted, setIsstarted] = useState(false);
   useEffect(() => {
     const roomRef = ref(db, `rooms/${roomId}/isStarted`);
     const unsub = onValue(roomRef, (snapshot) => {
       if (snapshot.val()) {
-        navigate(`/game/play/${roomId}/${userId}`)
-        console.log("Navigated");
+        setIsstarted(true);
       }
     });
     return unsub;
@@ -37,6 +39,41 @@ export default function UserLobby() {
   }, []);
   return (
     <section className="w-full grid-rows-8 md:grid-rows-none h-full grid md:grid-cols-4 gap-3">
+      {createPortal(
+        isStarted && (
+          <div className="w-screen h-screen   top-0 fixed">
+            <motion.div
+              initial={{
+                y: "-100%",
+              }}
+              animate={{
+                y: 0,
+                transition: {
+                  duration: 1,
+                },
+              }}
+              className="bg-black z-100 h-1/2 w-full"
+            ></motion.div>
+            <motion.div
+              initial={{
+                y: "100%",
+              }}
+              animate={{
+                y: 0,
+                transition: {
+                  duration: 1,
+                },
+              }}
+              onAnimationComplete={() => {
+                navigate(`/game/play/${roomId}/${userId}`);
+                console.log("Navigated");
+              }}
+              className="bg-black z-100 h-1/2 w-full"
+            ></motion.div>
+          </div>
+        ),
+        document.body,
+      )}
       <Invitations />
       <Players />
     </section>
--- src/components/Game/play/UserinGame.jsx
+++ src/components/Game/play/UserinGame.jsx
@@ -1,11 +1,15 @@
-import { onValue, ref } from "firebase/database";
-import { useEffect } from "react";
+import { get, onValue, ref } from "firebase/database";
+import { useEffect, useRef, useState } from "react";
 import { db } from "../../../config/firebase";
 import { useNavigate, useParams } from "react-router-dom";
-
+import { createPortal } from "react-dom";
+import { AnimatePresence, motion } from "motion/react";
 export default function UserInGame() {
   const { roomId, userId } = useParams();
   const navigate = useNavigate();
+  const [animationFinshed, setAnimationFinshed] = useState(false);
+  const [removePortal, setRemovePortal] = useState(false);
+  const [role, setRole] = useState(null);
   useEffect(() => {
     const isStartedRef = ref(db, `rooms/${roomId}/isStarted`);
     const unSub = onValue(isStartedRef, (snapshot) => {
@@ -14,7 +18,83 @@ export default function UserInGame() {
         navigate(`/game/lobby/${roomId}/${userId}`);
       }
     });
+
     return unSub;
   }, []);
-  return <p>play</p>;
+  const getRole = async () => {
+    const roleRef = ref(db, `rooms/${roomId}/players/${userId}/role`);
+    const role = await get(roleRef);
+    setRole(role.val());
+    setTimeout(() => {
+      setAnimationFinshed(true);
+    }, 10000);
+    setTimeout(() => {
+      setRemovePortal(true);
+    }, 11500);
+  };
+  useEffect(() => {
+    getRole();
+  }, []);
+  return (
+    <p>
+      {!removePortal &&
+        createPortal(
+          <div className="w-screen h-screen top-0 fixed">
+            <AnimatePresence>
+              {!animationFinshed && (
+                <>
+                  <motion.p
+                    initial={{
+                      opacity: 0,
+                      x: -50,
+                    }}
+                    animate={{
+                      opacity: 1,
+                      x: 0,
+                    }}
+                    transition={{
+                      duration: 1,
+                    }}
+                    exit={{
+                      opacity: 0,
+                    }}
+                    className={`
+          ${
+            role === "mafia"
+              ? "text-Im1"
+              : role === "doctor"
+                ? "text-blue-500"
+                : "text-font"
+          } z-100 font-Jungle absolute top-1/2 
+          left-1/2 text-8xl -translate-1/2 select-none`}
+                  >
+                    {role}
+                  </motion.p>
+                  <motion.div
+                    className="bg-black z-100 h-1/2 w-full"
+                    exit={{
+                      y: "-100%",
+                    }}
+                    transition={{
+                      duration: 1,
+                    }}
+                  ></motion.div>
+                  <motion.div
+                    exit={{
+                      y: "100%",
+                    }}
+                    transition={{
+                      duration: 1,
+                    }}
+                    className="bg-black z-100 h-1/2 w-full"
+                  ></motion.div>
+                </>
+              )}
+            </AnimatePresence>
+          </div>,
+          document.body,
+        )}
+      play
+    </p>
+  );
 }
