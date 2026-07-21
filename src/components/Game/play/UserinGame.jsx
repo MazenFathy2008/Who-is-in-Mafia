0d6dd5946be4b59eda33f2898bb9f839c185b070
mazenDev <mazenfathy841@gmail.com>
2026-07-21 19:38:23 +0300

    fixing the bug that appeared in the animation (Ai gives me the idea of how to fix it)

--- src/components/Game/play/UserinGame.jsx
+++ src/components/Game/play/UserinGame.jsx
@@ -8,9 +8,8 @@ import MafiaTable from "./MafiaTable";
 export default function UserInGame() {
   const { roomId, userId } = useParams();
   const navigate = useNavigate();
-  const [startWake, setStartWake] = useState(false);
-  const [animationFinshed, setAnimationFinshed] = useState(false);
-  const [removePortal, setRemovePortal] = useState(false);
+  const animations = ["role", "wake", "finshed"];
+  const [animateState, setanimateState] = useState(animations[0]);
   const [role, setRole] = useState(null);
   useEffect(() => {
     const isStartedRef = ref(db, `rooms/${roomId}/isStarted`);
@@ -27,106 +26,121 @@ export default function UserInGame() {
     const roleRef = ref(db, `rooms/${roomId}/players/${userId}/role`);
     const role = await get(roleRef);
     setRole(role.val());
-    setTimeout(() => {
-      setStartWake(true);
-    }, 5000);
-    setTimeout(() => {
-      setAnimationFinshed(true);
-    }, 10000);
-    setTimeout(() => {
-      setRemovePortal(true);
-    }, 12000);
   };
   useEffect(() => {
     getRole();
+    const t1 = setTimeout(() => {
+      setanimateState(animations[1]);
+      const t2 = setTimeout(() => {
+        setanimateState(animations[2]);
+      }, 5000);
+      return () => {
+        clearTimeout(t2);
+      };
+    }, 5000);
+    return () => {
+      clearTimeout(t1);
+    };
   }, []);
   return (
     <div className="w-full h-full overflow-hidden">
-      {!removePortal &&
-        createPortal(
-          <div className="w-screen h-screen top-0 fixed">
-            <AnimatePresence>
-              {!animationFinshed && (
-                <>
-                  <AnimatePresence mode="wait">
-                    {!startWake? (
-                      <motion.p
-                        key="role"
-                        initial={{
-                          opacity: 0,
-                          x: -50,
-                        }}
-                        animate={{
-                          opacity: 1,
-                          x: 0,
-                        }}
-                        transition={{
-                          duration: 1.5,
-                        }}
-                        exit={{
-                          opacity: 0,
-                          x: 50,
-                        }}
-                        className={`
-            ${
-              role === "mafia"
-                ? "text-Im1"
-                : role === "doctor"
-                  ? "text-blue-500"
-                  : "text-font"
-            } z-100 font-Jungle absolute top-1/2 
-            left-1/2 text-8xl -translate-1/2 select-none`}
-                      >
-                        {role}
-                      </motion.p>
-                    ) :  (
-                      <motion.p
-                        key="wake"
-                        initial={{
-                          opacity: 0,
-                          x: -50,
-                        }}
-                        animate={{
-                          opacity: 1,
-                          x: 0,
-                        }}
-                        transition={{
-                          duration: 0.5,
-                        }}
-                        exit={{
-                          opacity: 0,
-                        }}
-                        className="flex items-center justify-center w-full z-100 font-Jungle absolute top-1/2 
-            left-1/2 text-8xl -translate-1/2 select-none text-font"
-                      >
-                        All the city wake up
-                      </motion.p>
-                    )}
-                  </AnimatePresence>
-                  <motion.div
-                    className="bg-black z-100 h-1/2 w-full"
-                    exit={{
-                      y: "-100%",
-                    }}
-                    transition={{
-                      duration: 0.5,
-                    }}
-                  ></motion.div>
-                  <motion.div
-                    exit={{
-                      y: "100%",
-                    }}
-                    transition={{
-                      duration: 1,
-                    }}
-                    className="bg-black z-100 h-1/2 w-full"
-                  ></motion.div>
-                </>
+      {createPortal(
+        <div className="w-screen h-screen top-0 fixed">
+          <>
+            <AnimatePresence mode="wait">
+              {animateState === animations[0] ? (
+                <motion.p
+                  key={animations[0]}
+                  initial={{
+                    opacity: 0,
+                    x: -50,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    x: 0,
+                  }}
+                  transition={{
+                    duration: 1.5,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    x: 50,
+                  }}
+                  className={`
+              ${
+                role === "mafia"
+                  ? "text-Im1"
+                  : role === "doctor"
+                    ? "text-blue-500"
+                    : "text-font"
+              } z-100 font-Jungle absolute top-1/2 
+                  left-1/2 text-8xl -translate-1/2 select-none`}
+                >
+                  {role}
+                </motion.p>
+              ) : animateState === animations[1] ? (
+                <motion.p
+                  key={animations[1]}
+                  className="z-100 font-Jungle absolute top-1/2 
+                  left-1/2 md:text-8xl 
+                  -translate-1/2 
+                  select-none 
+                  text-font 
+                  w-full flex items-center justify-center text-4xl"
+                  initial={{
+                    opacity: 0,
+                    x: -50,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    x: 0,
+                  }}
+                  transition={{
+                    duration: 1.5,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    x: 50,
+                  }}
+                >
+                  All the city wake up
+                </motion.p>
+              ) : (
+                ""
               )}
             </AnimatePresence>
-          </div>,
-          document.body,
-        )}
+            {}
+            <AnimatePresence>
+              {animateState != animations[2] && (
+                <motion.div
+                  className="bg-black absolute z-99 h-1/2 w-full"
+                  exit={{
+                    y: "-100%",
+                  }}
+                  transition={{
+                    duration: 1.5,
+                  }}
+                ></motion.div>
+              )}
+            </AnimatePresence>
+
+            <AnimatePresence>
+              {animateState != animations[2] && (
+                <motion.div
+                  exit={{
+                    y: "100%",
+                  }}
+                  transition={{
+                    duration: 1.5,
+                  }}
+                  className="bg-black absolute bottom-0 z-99 h-1/2 w-full"
+                ></motion.div>
+              )}
+            </AnimatePresence>
+          </>
+        </div>,
+        document.body,
+      )}
       <MafiaTable />
     </div>
   );
